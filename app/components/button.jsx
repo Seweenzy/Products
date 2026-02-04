dba3fd42b697ec2cc0ac6278c3ddfd108cfb41fa
Seweenzy <koluwaseunemmanuel@gmail.com>
2026-02-04 13:40:46 -0800

    init

---
+++ app/components/button.jsx
@@ -0,0 +1,20 @@
+import { TouchableOpacity, Text } from "react-native"
+
+
+
+const Button =({text, style})=>{
+
+    return(
+        <TouchableOpacity style={{backgroundColor:'purple', height:50,
+        borderRadius:12, justifyContent:'center', ...style }}
+        > 
+
+            <Text style={{alignSelf:'center', fontSize:15, color: 'white'}}>{text}</Text>
+
+        </TouchableOpacity>
+
+
+    )
+}
+
+export default Button;
